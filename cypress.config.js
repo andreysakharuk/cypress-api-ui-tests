d5391c39ba19f458b069a470955fc3254031b783
andreysakharuk <andreysakharuk@gmail.com>
2024-04-04 17:56:02 +0200

    baseurl update

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const cypressSplit = require("cypress-split");
 
 module.exports = defineConfig({
     e2e: {
+        baseUrl: "https://app.todoist.com",
         setupNodeEvents(on, config) {
             cypressSplit(on, config);
             return config;
